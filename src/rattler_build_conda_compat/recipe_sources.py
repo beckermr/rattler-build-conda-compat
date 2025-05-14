82c388eeac9d8bc0fc975302a76b689f62c6e5a3
Yannik Tausch <dev@ytausch.de>
2025-05-14 21:08:34 +0200

    fix context variables bug, add test for variants in sources (#66)

--- src/rattler_build_conda_compat/recipe_sources.py
+++ src/rattler_build_conda_compat/recipe_sources.py
@@ -125,7 +125,7 @@ def render_all_sources(  # noqa: C901
         if isinstance(template, list):
             return [cast(str, render(t, context)) for t in template]
         template = env.from_string(template)
-        return template.render(context_variables)
+        return template.render(context)
 
     if override_version is not None:
         recipe["context"]["version"] = override_version
